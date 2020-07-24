e2c6398c37db02ed71716440a155720cc15403bc
Layon Martins <layonmartins03@gmail.com>
2020-07-24 16:19:49 -0300

    exercise 05

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -43,8 +43,8 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // TODO (04) Switch to inflating FragmentOverviewBinding
-        val binding = GridViewItemBinding.inflate(inflater)
+        // TODOne (04) Switch to inflating FragmentOverviewBinding
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner(this)
@@ -52,7 +52,8 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
-        // TODO (12) Set binding.photosGrid.adapter to a new PhotoGridAdapter()
+        // TODOne (12) Set binding.photosGrid.adapter to a new PhotoGridAdapter()
+        binding.photosGrid.adapter = PhotoGridAdapter()
         setHasOptionsMenu(true)
         return binding.root
     }
